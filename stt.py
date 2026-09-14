17021456e420172caa25e2ce1913b1bfebe7eb59
daveadi04 <adityadave004@gmail.com>
2026-09-14 19:12:28 -0400

    changed name

--- stt.py
+++ stt.py
@@ -2,7 +2,7 @@
 import whisper
 # using large model as on github it is mentioned that large model is the best for accuracy and turbo is not adviced.
 model = whisper.load_model("large-v2")
-result = model.transcribe(audio = "audios/6_SEO and Core Web Vitals in HTML.mp3.mp3",
-                          translate=True,
-                          language="hi")
+result = model.transcribe(audio = "audios/4_test.mp3",
+                          language="hi",
+                          task="translate")
 print(result["text"])
