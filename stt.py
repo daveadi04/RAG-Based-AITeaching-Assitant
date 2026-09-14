d13f633a1702c2d913d9886a090941b96e71a7af
daveadi04 <adityadave004@gmail.com>
2026-09-14 19:47:11 -0400

    created chunks with small video and dumped it into json file

--- stt.py
+++ stt.py
@@ -1,8 +1,18 @@
 #for trabnslating speach to text
 import whisper
+import json
+
 # using large model as on github it is mentioned that large model is the best for accuracy and turbo is not adviced.
 model = whisper.load_model("large-v2")
 result = model.transcribe(audio = "audios/4_test.mp3",
                           language="hi",
-                          task="translate")
-print(result["text"])
+                          task="translate",
+                          without_timestamps=False)
+print(result["segments"])
+chunks = []
+for segment in result["segments"]:
+    chunks.append({"id" : segment["id"], "start": segment["start"], "end": segment["end"], "text": segment["text"]})
+
+print(chunks)
+with open("output.json", "w") as f:
+    json.dump(chunks,f)
